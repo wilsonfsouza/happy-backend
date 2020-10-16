0aeb284b7c1b5176733886d7864f302699da73a8
Wilson Franca <wilson.franca.92@gmail.com>
2020-10-15 19:19:53 -0700

    added router and multer middleware to upload an array of images

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 import FacilitiesController from '../controllers/FacilitiesController';
 
 const routes = Router();
-
-routes.post('/facilities', FacilitiesController.create);
+const upload = multer(uploadConfig);
+routes.get('/facilities', FacilitiesController.index);
+routes.get('/facilities/:id', FacilitiesController.show);
+routes.post('/facilities', upload.array('images'), FacilitiesController.create);
 
 export default routes;
