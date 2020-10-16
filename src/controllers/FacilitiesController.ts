ed27031c8cd8faf24aa99d3688720e8284ed2835
Wilson Franca <wilson.franca.92@gmail.com>
2020-10-15 19:22:26 -0700

    added methods index, show, and create

--- src/controllers/FacilitiesController.ts
+++ src/controllers/FacilitiesController.ts
@@ -1,8 +1,25 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import orphanageView from '../views/facilities_view';
+import * as Yup from 'yup';
 import Facility from '../models/Facility';
 
 export default {
+  async index(request: Request, reponse: Response) {
+    const facilitiesRepository = getRepository(Facility);
+    const facilities = await facilitiesRepository.find({
+      relations: ['images']
+    });
+    return reponse.json(orphanageView.renderMany(facilities));
+  },
+  async show(request: Request, reponse: Response) {
+    const { id } = request.params;
+    const facilitiesRepository = getRepository(Facility);
+    const facility = await facilitiesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
+    return reponse.json(orphanageView.render(facility));
+  },
   async create(request: Request, reponse: Response) {
     const {
       name,
@@ -16,15 +33,37 @@ export default {
 
     const facilitiesRepository = getRepository(Facility);
 
-    const facility = facilitiesRepository.create({
+    const requestImages = request.files as Express.Multer.File[];
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
+    const data = {
       name,
       latitude,
       longitude,
       about,
       instructions,
       open_hours,
-      open_on_weekends
+      open_on_weekends,
+      images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      open_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(Yup.object().shape({
+        path: Yup.string().required(),
+      })),
     });
+
+    await schema.validate(data, { abortEarly: false })
+
+    const facility = facilitiesRepository.create(data);
 
     await facilitiesRepository.save(facility);
 
